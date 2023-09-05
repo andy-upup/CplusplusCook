93853f5ca0c46eb495fc7b601413aa2f109a3878
andy <2394635898@qq.com>
2023-09-05 23:21:54 +0800

    add shell && update cmake

---
+++ scripts/build_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+root_path=$(cd $(dirname $0); cd ..; pwd)
+cd ${root_path}
+
+linux_build_path=${root_path}/build_linux
+if [ -d ${linux_build_path} ]; then
+    rm -rf ${linux_build_path}
+fi
+
+mkdir -p ${linux_build_path}
+
+cd ${linux_build_path}
+cmake -DBUILD_TEST=ON                            \
+      -DCMAKE_INSTALL_PREFIX=${linux_build_path} \
+..
+
+make -j4
+make install
---
+++ scripts/run_linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+root_path=$(cd $(dirname $0); cd ..; pwd)
+
+source ${root_path}/scripts/build_linux.sh
+
+cd ${linux_build_path}/test/bin
+./gtest_sample --gtest_filter=gtest_sample.print
