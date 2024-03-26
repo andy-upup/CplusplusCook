d7e348453b9ddc6446b9a6790ebdbe0389152587
andy <2394635898@qq.com>
2024-03-26 20:35:22 +0800

    algo: add pipeline

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -17,7 +17,7 @@ if [ ${execute_mode} == "CPP" ]; then
     ./cpp11_test_case --gtest_filter=test11_cast.cast_unit_test
 elif [ ${execute_mode} == "ALGO" ]; then
     cd ${linux_build_path}/install/bin/algorithm
-    ./graph_test_case --gtest_filter=test_algo_graph.knn_unit_test
+    ./others_test_case --gtest_filter=test_algo_others.pipeline_unit_test
 elif [ ${execute_mode} == "DP" ]; then
     cd ${linux_build_path}/install/bin/design_pattern
     ./singleton_test_case --gtest_filter=test_design_pattern.singleton_test_case
--- src/algorithm/test/others_test_case.cpp
+++ src/algorithm/test/others_test_case.cpp
@@ -18,3 +18,13 @@ TEST(test_algo_others, juice_unit_test) {
     std::cout << "9: " << algo::JuiceNum(9) << std::endl;
     std::cout << "10: " << algo::JuiceNum(10) << std::endl;
 }
+
+TEST(test_algo_others, pipeline_unit_test) {
+    std::vector<algo::Instruction> program{
+        algo::Instruction(0), algo::Instruction(1),
+        algo::Instruction(2), algo::Instruction(3),
+        algo::Instruction(4), algo::Instruction(5),
+        algo::Instruction(6)};
+    algo::ThreeStagePipeline pipeline;
+    pipeline.Run(program);
+}
